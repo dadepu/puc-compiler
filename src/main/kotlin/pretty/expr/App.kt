ad30c2b1d3612003cd730d04e6e300fe62eb496c
dadepu <84119713+dadepu@users.noreply.github.com>
2022-06-09 19:00:06 +0200

    doc: possible App use-cases

--- src/main/kotlin/pretty/expr/App.kt
+++ src/main/kotlin/pretty/expr/App.kt
@@ -5,45 +5,77 @@ import pretty.Format
 import pretty.Line
 import pretty.LineMode
 import pretty.Printable
+import pretty.functions.parseExpr
 
+/*
+    Possible App occurrences:
+
+    # Top Level
+
+        (1)     let add = \x -> \y -> x + y
+                add (1) (2)
+
+
+    # Embedded
+
+        (1)     \x -> \y -> x + y (1) (2)
+
+        (2)     \x ->
+                    \y ->
+                        x + y
+                (1) (2)
+
+        (3)     \x ->
+                    \y ->
+                        x + y
+                (1 + 1 + 1 ...
+                    ...) (2)
+
+        (4)     \x -> \y -> x + y (1 + 1 + 1 ...
+                    ...) (2)
+
+        (5)     \x -> \y -> x + y ..
+                (1) (2)
+
+        (6)     if \x -> 3 + x (1) == 4 then
+                    3
+                else
+                    \x -> x + 1 (1)
+
+        (7)     if \x -> \y -> x + y (1) (2)
+                    == 3
+                then
+                    \x ->
+                        \y ->
+                            x + y ...
+                    (1) (2 + ...
+                        ...)
+                else
+                    3
+
+        (8)     let a = \x ->
+                    \y ->
+                        let b = \i -> \j -> j + \z -> z + 1 (1)
+                        b (1) (2)
+                (3) (4)
+ */
 data class App(
 
     val content: Expr.App
 
 ) : Printable {
 
+    private val exprFunc: Printable = parseExpr(content.func)
+
+    private val exprArg: Printable = parseExpr(content.arg)
+
+    // TODO
     override fun generateOutput(f: (LineMode) -> Format): Pair<LineMode, List<Line>> {
-        TODO("Not yet implemented")
+        return Pair(
+            LineMode.SINGLE,
+            listOf(
+                Line(0, "${exprFunc.generateOutput(f).second.first().content} (${exprArg.generateOutput(f).second.first().content})")
+            )
+        )
     }
 }
-
-    //    override val flatLength: Int
-//        get() = flattenOutput(firstLine)(singleLineOutput).length
-//
-//    override val isMultiline: Boolean
-//        get() = false
-//
-//    override fun modifyIndentRelatively(x: Int): Printable {
-//        return this.copy(indent = indent + 1)
-//    }
-//
-//    override val output: List<Line>
-//        get() = singleLineOutput
-//
-//    // add 3
-//    private val singleLineOutput: List<Line>
-//        get() {
-//            val func = flattenOutput(concatLines)(parseExpr(0)(content.func).generateOutput)
-//            val arg = flattenOutput(concatLines)(parseExpr(0)(content.arg).generateOutput)
-//            return listOf(Line(indent, "$func $arg"))
-//        }
-//
-//    // ... add \x ->
-//    //     3 + 3
-//    private val sameLineMultiLineOutput: List<Line>
-//        get() = TODO()
-//
-//    // ... add
-//    //     \x -> 3 + 3
-//    private val multiLineOutput: List<Line>
-//        get() = TODO()
